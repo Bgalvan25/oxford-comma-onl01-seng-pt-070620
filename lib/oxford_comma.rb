a09888d258d94e18be4f783b5a0a85e57b82f0f2
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-15 01:08:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if # The length of the array is equaled to 1
+    # What should we do here?
+    
+  elsif # The length of the array is equaled to 2
+    # What should we do here instead?
+    
+  elsif # The length of the array is equaled to 3 or more
+    # What should we do here instead?
+  end
 end
